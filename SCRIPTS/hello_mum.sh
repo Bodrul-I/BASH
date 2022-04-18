ae6a48cdf87a7344583ec24d7c97d60fd59eb7ef
Bodrul-I <bodrul.islam93@googlemail.com>
2022-04-18 03:28:39 +0100

    Edited hello_mum.sh file

--- SCRIPTS/hello_mum.sh
+++ SCRIPTS/hello_mum.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 
-echo "Hello Mum"
-sleep 3 
-echo "How are you?"
-sleep 3 
-echo "How's dad doing?"
+echo "Hello Mum and Dad"
+sleep 2 
+echo "How are you doing?"
 sleep 2
 echo "I'm doing well be home soon"
 
